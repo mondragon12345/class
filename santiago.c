d295830ebcab22ebfca6af0dd09dae88071370fd
mondragon12345 <davidmondragon@unicomfacauca.edu.co>
2021-04-15 22:03:35 -0500

    santiago

--- santiago.c
+++ santiago.c
@@ -1,1 +1,24 @@
-prueba santiago 
+#include <stdio.h>
+//santiago
+int main()
+{
+ int vector[10]={10,2,3,45,8,45,4,7,6,9};
+ int aux;
+ int i,pocicon=0;
+ for (int i = 0; i <=3; i++){
+   for(int l = 0; l < 3; l++){
+     if(vector[l] > vector[l+1])
+      aux = vector [l+1];
+      vector[l+1] = vector[l+1];
+      vector [l] = aux;
+     }
+   }
+ }
+
+ for (int i = 3; i >=0; i++){
+   printf("%d\t",vector[i]);
+ }
+
+retun 0;
+
+}
