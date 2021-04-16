39c407e90b7f097f7fb1fdbb86d12a558e0f81e9
xlsirius <67179516+xlsirius@users.noreply.github.com>
2021-04-16 00:06:45 -0500

    terminado

--- santiago.c
+++ santiago.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
-//santiago
-//javier
+#include <stdlib.h>
+#include <math.h>
+
 int main()
 {
- int vector[10]={10,2,3,45,8,45,4,7,6,9};
- int aux;
- int i,pocicon=0;
- for (int i = 0; i <=3; i++){
-   for(int l = 0; l < 3; l++){
-     if(vector[l] > vector[l+1])
-      aux = vector [l+1];
-      vector[l+1] = vector[l+1];
-      vector [l] = aux;
-     }
-   }
- }
-
- for (int i = 3; i >=0; i++){
-   printf("%d\t",vector[i]);
- }
-
-retun 0;
-
+    int vector[10]={10,2,3,45,8,45,4,7,6,9};
+    int i,j,x,y;
+    printf(":: Numeros a ordenar :: \n");
+    for (i = 0; i <=10; i++)
+    {
+      printf("%d",vector[i]);
+      printf(",");
+    }
+    for(i=0;i<=10;i++)
+    {
+        for(j=i+1;j<=10;j++)
+        {
+            x = vector[i] - vector[j];
+            if(x>0)
+            {
+                y = vector[j];
+                vector[j] = vector[i];
+                vector[i] = y;
+            }
+        }
+    }
+    printf("\n---------------------------------\n");
+    printf(" \n **Los numeros organizados** \n");
+    for(i=0;i<=10;i++)
+    {
+        printf("%d", vector[i]);
+        printf(",");
+    }
 }
