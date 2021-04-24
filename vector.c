a9bf4f54adfd449da7706487e31d563af1eb9d8f
mondragon12345 <davidmondragon@unicomfacauca.edu.co>
2021-04-23 21:03:15 -0500

    ventor

---
+++ vector.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  double ope,aux;
+  ope= 2+3;
+  aux= ope*sin (25);
+  printf(":::EJERCICIO DE Seno:::\n");  
+  printf("Resultado de 2+3* sen (25) es: %d \n",ope);
+}
